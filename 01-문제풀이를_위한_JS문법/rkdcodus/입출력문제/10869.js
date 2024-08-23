a26fae19fe53252a81d079f4ffff49ea16a78944
rkdcodus <rcd7275@naver.com>
2024-08-24 00:50:09 +0900

    기초문제

---
+++ 01-문제풀이를_위한_JS문법/rkdcodus/입출력문제/10869.js
@@ -0,0 +1,15 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const a = input[0];
+const b = input[1];
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
