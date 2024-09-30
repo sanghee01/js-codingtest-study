869a883ca836f70a265bd55ff2fd5e4b7f1f6b66
sanghee01 <sang6299@naver.com>
2024-09-30 21:40:01 +0900

    백트래킹 9/30

---
+++ 06-백트래킹/이상희/n-queen.js
@@ -0,0 +1,26 @@
+const n = Number(require("fs").readFileSync("../../input.txt").toString());
+let queens = [];
+
+function possible(x, y) {
+  for (let [a, b] of queens) {
+    if (a == x || b == y) return false;
+    if (Math.abs(a - x) == Math.abs(b - y)) return false;
+  }
+  return true;
+}
+
+let cnt = 0;
+function dfs(row) {
+  if (row == n) {
+    cnt += 1;
+    return;
+  }
+  for (let i = 0; i < n; i++) {
+    if (!possible(row, i)) continue;
+    queens.push([row, i]);
+    dfs(row + 1);
+    queens.pop();
+  }
+}
+dfs(0);
+console.log(cnt);
